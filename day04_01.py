397469adf2d087ba9c3f0b14b084e45a6d401021
RaihanKabir277 <raihank0198@gmail.com>
2025-04-16 07:45:29 +0600

    random with head tails game , random + list with randomly select one friends problem added here for day 04

---
+++ day04_01.py
@@ -0,0 +1,35 @@
+import random
+
+# import my_module
+#
+#
+#
+# random_int = random.randint(1, 10)
+# print(random_int)
+# print(my_module.my_fav_number)
+
+
+# random_number_0_to_1 = random.random()
+# print(random_number_0_to_1)
+
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(1, 10)
+# print(random_float)
+
+# --------heads or tails problem --------
+
+heads_or_tails = random.randint(0, 1)
+
+if heads_or_tails == 0:
+    print("Heads")
+
+else:
+    print("tails")
+
+
+
+
+
+
---
+++ day04_03.py
@@ -0,0 +1,31 @@
+
+# ------------randomly select a bank card from a friend for paying bill problem -----------
+
+import random
+
+# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# print(random.choice(friends))
+
+# ------------- another way ----------------
+
+# friends = ["Nazir", "Nabil", "rifta", "gourab", "hridoy"]
+
+# last = len(friends)
+
+# random_index = random.randint(0, last-1)
+# print(friends[random_index])
+
+
+# -------------nested lists ----------
+
+fruits = ["mango", "apple", "lichi"]
+vegetables = ["potato", "carrots", "bins"]
+
+dirty_dozen = [fruits, vegetables]
+
+# print(dirty_dozen)
+
+print(dirty_dozen[1][1])
+
+

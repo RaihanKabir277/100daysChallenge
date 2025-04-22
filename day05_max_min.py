6180ad41076590ee0383736cff0076f5cb729c5d
RaihanKabir277 <raihank0198@gmail.com>
2025-04-22 09:54:02 +0600

    min added without using min build in function

--- day05_max_min.py
+++ day05_max_min.py
@@ -1,22 +1,41 @@
 
 
-n = int(input("Give me the number of number you want to add here : "))
+# n = int(input("Give me the number of number you want to add here : "))
 
-scores = []
-temp = 0
+# scores = []
+# temp = 0
+
+# for i in range(n):
+
+#     scores.append(int((input(f"give me the {i + 1} score : "))))
+# #    take = input(f"Give me the {i+1} scores : ")
+# #    scores.append(take)
+
+
+# print(scores)
+
+# for score in scores :
+#         if score > temp :
+#                temp = score
+    
+# print(temp)
+    
+
+#      find min without min function -------------
+
+n = int(input("give me the max number of inputs you want to push : "))
+
+min_scores = []
 
 for i in range(n):
+    min_scores.append(float(input(f"Enter the {i+1} scores : ")))
 
-    scores.append(int((input(f"give me the {i + 1} score : "))))
-#    take = input(f"Give me the {i+1} scores : ")
-#    scores.append(take)
+print(min_scores)
+temp = min_scores[0]
 
+for score in min_scores : 
+    if score <= temp :
+        temp = score
 
-print(scores)
-
-for score in scores :
-        if score > temp :
-               temp = score
-    
 print(temp)
-    
+

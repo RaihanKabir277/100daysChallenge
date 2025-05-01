8f9c08759387db1e4f1cd2cf5f05633dd56feea4
RaihanKabir277 <raihank0198@gmail.com>
2025-05-01 11:19:53 +0600

    day 08 function added here

---
+++ day_08_function.py
@@ -0,0 +1,28 @@
+
+# def great():
+#     print("Hellow")
+#     print("Hpw are you")
+
+# great()
+
+# def life_in_weeks(age):
+#     remain = (90 - age) * 52
+#     print(f"You have {remain} weeks left")
+
+
+
+# life_in_weeks(56)
+
+# def great_with(name, location):
+#     print(f"hellow {name}")
+#     print(f"What is your location {location}")
+
+# great_with("Raihan", "Dhaka")
+
+# def great_with(name, location):
+#     print(f"hellow {name}")
+#     print(f"your location {location}")
+
+# great_with(location="Dhaka", name="Kabir")
+
+

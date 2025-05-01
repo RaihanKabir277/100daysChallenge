824735153bc384655d73f2d745c0ca1a49392a66
RaihanKabir277 <raihank0198@gmail.com>
2025-05-01 22:50:25 +0600

    caesar cipher fully added here with lots of deifferent scenarious

--- day08_CaeserCipher.py
+++ day08_CaeserCipher.py
@@ -1,9 +1,9 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
 
 # def encrypt(original_text, shift_amount):
 #     encrypted_text = ""
@@ -35,17 +35,53 @@ shift = int(input("Type the shift number:\n"))
 
 def caesar(original_text, shift_amount, encode_or_decode):
     encrypted_decrypted_text = ""
-    for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
-        elif encode_or_decode == "encode":
-            shift_amount *= +1
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-
-        encrypted_decrypted_text += alphabet[shifted_position]
-
-    print(f"Here is the encoded text : {encrypted_decrypted_text}")
-
-caesar(text, shift, direction)
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+            
+    elif encode_or_decode == "encode":
+        shift_amount *= +1
     
+            
+    for letter in original_text:
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+
+            encrypted_decrypted_text += alphabet[shifted_position]
+        else:
+            encrypted_decrypted_text += letter
+        
+
+    print(f"Here is the {encode_or_decode}d text : {encrypted_decrypted_text}")
+
+# caesar(text, shift, direction)
+
+
+# while True:
+#     again = input("Enter Yes if you want to again otherwise enter No :\n").lower()
+#     if again == "yes":
+#         direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+#         text = input("Type your message:\n").lower()
+#         shift = int(input("Type the shift number:\n"))
+#         caesar(text, shift, direction)
+#     elif again == "no":
+#         print("Goodbye!")
+#         break
+#     else:
+#         print("You enter a wrong things ....")
+
+#  ---------  more sufficisnt way of using where loop here -----------
+
+again = True
+while again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, direction)
+
+    restart = input("Enter Yes if you want to again otherwise enter No :\n").lower()
+
+    if restart == "no":
+        again = False
+        print("Goodbye....")
+        

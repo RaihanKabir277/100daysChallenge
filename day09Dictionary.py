a1c0a6ffeee19cd692363472ae227636b2849a58
RaihanKabir277 <raihank0198@gmail.com>
2025-05-02 22:03:56 +0600

    Class 09 dictionary basic added here with a practise problem

---
+++ day09Dictionary.py
@@ -0,0 +1,23 @@
+
+programing_dictionary = {
+    "Bug" : "Is there is any bug",
+    "Function" : "Called me if any",
+    "Loop" : "hlw world",
+}
+
+# print(programing_dictionary["Bug"])
+
+programing_dictionary["Loop"] = "Kire kire"
+# print(programing_dictionary["Loop"])
+
+empty_dictionary = {}
+
+programing_dictionary["Problem"] = "No problem"
+
+# print(programing_dictionary)
+
+for key in programing_dictionary:
+    print(key)
+    print(programing_dictionary[key])
+
+

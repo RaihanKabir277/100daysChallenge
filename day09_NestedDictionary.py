c1e6ed4dadffe8d687a6e192bc5d00443349e275
RaihanKabir277 <raihank0198@gmail.com>
2025-05-02 22:31:35 +0600

    Nested Dictionary added here

---
+++ day09_NestedDictionary.py
@@ -0,0 +1,32 @@
+
+capitals = {
+    "France" : "Paris",
+    "Germany" : "Berlin",
+}
+
+travel_log = {
+    "France" : ["Paris", "Lille", "Dijon"],
+    "Germany" : ["Stuttgart", "Berlin"],
+}
+
+# print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]  #here A =0 B = 1, C,D = 2
+
+# print(nested_list[2][0])
+
+travel_log1 = {
+    "France" : {
+        "Cities_Visited" : ["Paris", "Lille", "Dijon"],
+        "Total_Visited"  : 12
+    },
+    "Germany" : {
+        "Cities_Visited" : ["Berlin", "Hamburg", "Stuttgart"],
+        "Total_Visited"  : 5
+
+    },
+}
+
+# print(travel_log1["France"]["Cities_Visited"][0])
+
+

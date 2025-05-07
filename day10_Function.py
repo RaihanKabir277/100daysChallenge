7b575f5c912203fb79e251a1f021c5f7045270dc
RaihanKabir277 <raihank0198@gmail.com>
2025-05-07 07:10:52 +0600

    multiple return function added here

--- day10_Function.py
+++ day10_Function.py
@@ -20,12 +20,27 @@
 
 # ---------------------- nested Function ---------
 
-def function1(text):
-    return text + " " + text
+# def function1(text):
+#     return text + " " + text
 
-def function2(text):
-    return text.title()
+# def function2(text):
+#     return text.title()
 
 
-output = function2(function1("hello"))
-print(output)
+# output = function2(function1("hello"))
+# print(output)
+
+
+# -----------Multiple return values ---------------
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return 
+
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result : {formated_f_name} {formated_l_name}"
+    
+
+print(format_name(input("What is your first name?\n "), input("What is your last name? \n")))
+

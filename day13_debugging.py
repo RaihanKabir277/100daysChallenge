656ca1ecd4e352d1567c57c9c43861f5474081a2
RaihanKabir277 <raihank0198@gmail.com>
2025-05-15 07:28:54 +0600

    debugging added here

--- day13_debugging.py
+++ day13_debugging.py
@@ -22,8 +22,41 @@
 
 # squash bug with a print --------
 
-word_per_page = 0
-pages = int(input("Number of pages: "))
-word_per_page = int(input("Number of words per page: "))
-total_words = pages * word_per_page
-print(total_words)
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+
+#  ---------------- Debugging  --------------
+
+# import random
+# import maths
+
+
+# def mutate(a_list):
+#     b_list = []
+#     new_item = 0
+#     for item in a_list:
+#         new_item = item * 2
+#         new_item += random.randint(1, 3)
+#         new_item += maths.add(new_item, item)
+#         b_list.append(new_item)
+#     print(b_list)
+
+
+# mutate([1, 2, 3, 5, 8, 13])
+
+# debug the fizzbuzz game --------
+
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0:
+            print("Fizz")
+        if number % 5 == 0:
+            print("Buzz")
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(number)

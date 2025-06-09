dad456b267633238e3cd96023f4eddcbde124cc5
RaihanKabir277 <raihank0198@gmail.com>
2025-06-09 17:09:34 +0600

    method, attributeobject creation with the package of Turtle

--- day16_OOP.py
+++ day16_OOP.py
@@ -9,8 +9,39 @@
 
 # Simplest way given below ------ 
 
-from turtle import Turtle
+# from turtle import Turtle
+
+# timmy = Turtle()
+# print(timmy)
+
+
+# Basic attributes starts here .......
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)  #canvheight is the Attributes of my screen......
+
+
+# Basic methods starts here ---------
+
+
+
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("red", "green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight) 
+my_screen.exitonclick()
+
+
+
 

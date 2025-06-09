c0e77b274b23d7f6025fef751ddfbb65207bf5aa
RaihanKabir277 <raihank0198@gmail.com>
2025-06-09 16:37:15 +0600

    starting the basic object and class from OOP

---
+++ day16_OOP.py
@@ -0,0 +1,16 @@
+
+# import day16_AnotherModule
+
+# print(day16_AnotherModule.another_variable)
+
+# import turtle
+
+# timmy = turtle.Turtle()
+
+# Simplest way given below ------ 
+
+from turtle import Turtle
+
+timmy = Turtle()
+print(timmy)
+

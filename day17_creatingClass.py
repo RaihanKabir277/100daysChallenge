ca49651ebf79222f3bee5335a319717203c7c82a
RaihanKabir277 <raihank0198@gmail.com>
2025-06-12 06:18:07 +0600

    basic creating class added here

---
+++ day17_creatingClass.py
@@ -0,0 +1,20 @@
+
+class User:
+    pass
+
+user_1 = User()
+user_1.id = "218"
+user_1.name = "Kabir"
+
+print(user_1.name)
+
+# user_2 = User()
+# user_2.id = "219"
+# user_2.name = "nabil"
+# print(user_2.id)
+
+# this is okay but if there is more than 100 of users then it will make complexity
+# so , overcome this complexity and make the code more easier we will use constructor...
+
+
+

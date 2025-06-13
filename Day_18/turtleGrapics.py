1997307e7b256a5b784eb26476db013f4bd6f8b5
RaihanKabir277 <raihank0198@gmail.com>
2025-06-13 06:23:16 +0600

    starting with turtle grapics documentation

---
+++ Day_18/turtleGrapics.py
@@ -0,0 +1,23 @@
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+# print(timmy)   #it will not show the gui screen so for contain the screen , we have to be imported Screem module
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()     # now we will see the full screen 
+
+

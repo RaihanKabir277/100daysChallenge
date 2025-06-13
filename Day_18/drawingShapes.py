c1c300e4023b2852c879d25ced9aadcfb751524e
RaihanKabir277 <raihank0198@gmail.com>
2025-06-13 20:10:48 +0600

    triangle, square, pentagon added with basic code

---
+++ Day_18/drawingShapes.py
@@ -0,0 +1,36 @@
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.forward(100)
+timmy.right(120)
+timmy.forward(100)
+timmy.right(120)
+timmy.forward(100)
+timmy.right(120)
+timmy.color("red")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.color("green")
+timmy.forward(100)
+timmy.right(72)
+timmy.forward(100)
+timmy.right(72)
+timmy.forward(100)
+timmy.right(72)
+timmy.forward(100)
+timmy.right(72)
+timmy.forward(100)
+timmy.right(72)
+
+
+
+screen = Screen()
+screen.exitonclick()

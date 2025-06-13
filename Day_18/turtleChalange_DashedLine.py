2502608e5cd0948feb5f49da66f4358bdf524a3d
RaihanKabir277 <raihank0198@gmail.com>
2025-06-13 19:44:53 +0600

    dashed line created by Turtle added here

---
+++ Day_18/turtleChalange_DashedLine.py
@@ -0,0 +1,16 @@
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+for _ in range(50):
+    timmy.forward(5)
+    timmy.penup()
+    timmy.forward(5)
+    timmy.pendown()
+
+
+
+
+screen = Screen()
+screen.exitonclick()

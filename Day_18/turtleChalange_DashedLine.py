ed3659b4ff2ab2c62b891e4e3ac0d6560fecba96
RaihanKabir277 <raihank0198@gmail.com>
2025-06-13 19:52:52 +0600

    another way added for drawing a dashed lines

--- Day_18/turtleChalange_DashedLine.py
+++ Day_18/turtleChalange_DashedLine.py
@@ -3,14 +3,18 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 
-for _ in range(50):
+# for _ in range(50):
+#     timmy.forward(5)    # moving forwared 5 paces
+#     timmy.penup()       #pen up no drawing now 
+#     timmy.forward(5)    # moving forward 5 paces without drawing anything
+#     timmy.pendown()     #then pen down that means it is now ready to draw
+
+
+for _ in range(20):
     timmy.forward(5)
-    timmy.penup()
+    timmy.color("white")
     timmy.forward(5)
-    timmy.pendown()
-
-
-
+    timmy.color("black")
 
 screen = Screen()
 screen.exitonclick()

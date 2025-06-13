57d2a583f022213ac4587212518db384cabdf29a
RaihanKabir277 <raihank0198@gmail.com>
2025-06-13 06:53:58 +0600

    draw a 100 * 100 simple square by using turtle module

--- Day_18/turtleGrapics.py
+++ Day_18/turtleGrapics.py
@@ -5,6 +5,17 @@ timmy = Turtle()
 
 # print(timmy)   #it will not show the gui screen so for contain the screen , we have to be imported Screem module
 
+# timmy.shape("turtle")
+timmy.shape("square")
+timmy.color("red", "green")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+# timmy.left(-90)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
 
 
 

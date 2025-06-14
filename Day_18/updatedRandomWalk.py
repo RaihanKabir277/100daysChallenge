9eeaaaf5d92d8e3ad6a515fe2030bec6efb40898
RaihanKabir277 <raihank0198@gmail.com>
2025-06-14 15:47:15 +0600

    updated random walk added here with speed up the drawing

--- Day_18/updatedRandomWalk.py
+++ Day_18/updatedRandomWalk.py
@@ -7,6 +7,13 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSea
             "wheat", "SlateGray", "SeaGreen"]
 
 directions = [0, 90, 180, 270]   #nort south east west 
+timmy.pensize(10)
+timmy.speed("fastest")
+
+for _ in range(200):
+    timmy.color(random.choice(colours))
+    timmy.forward(20)
+    timmy.setheading(random.choice(directions))
 
 
 screen = Screen()

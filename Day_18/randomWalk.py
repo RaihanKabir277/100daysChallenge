f2aef53bb8980df04f23f19851e3b0f9c480a49e
RaihanKabir277 <raihank0198@gmail.com>
2025-06-14 14:45:37 +0600

    sarting coding for random walk drawing generator

---
+++ Day_18/randomWalk.py
@@ -0,0 +1,21 @@
+
+from turtle import Turtle, Screen
+import random
+
+timmy = Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
+            "wheat", "SlateGray", "SeaGreen"]
+
+
+# for i in range(100):
+#     steps = int(random() * 100)
+#     angle = int(random() * 360)
+#     timmy.right(angle)
+#     timmy.forward(steps)
+
+
+
+
+screen = Screen()
+screen.exitonclick()

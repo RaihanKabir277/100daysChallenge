61ee91a6f34c81fc07514086009ed5d7913fd1ec
RaihanKabir277 <raihank0198@gmail.com>
2025-06-14 15:24:30 +0600

    added basic random walk here

--- Day_18/randomWalk.py
+++ Day_18/randomWalk.py
@@ -16,14 +16,26 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSea
 # timmy.pen(fillcolor="black", pencolor="red", pensize=10)
 # timmy.forward(100)
 
+def right():
+    return timmy.right(90)
+def left():
+    return timmy.left(90)
+
+actions = [right, left]
+
 timmy.pensize(10)
 
-for i in range(20):
-    timmy.forward(30)
-    timmy.left(90)         #stairs drawing code 
-    timmy.forward(30)
-    timmy.right(90)
+# for i in range(20):
+#     timmy.forward(30)
+#     timmy.left(90)         #stairs drawing code 
+#     timmy.forward(30)
+#     timmy.right(90)
     
+for n in range(30):
+    timmy.color(random.choice(colours))
+    timmy.forward(30)
+    random.choice(actions)()
+    timmy.forward(30)
 
 
 
